aec40bc5a64f4e7af812968095db5157ac0e2f6e
leonmaia <leon@leonmaia.com>
2013-02-16 09:42:43 -0300

    Adicionando um ViewController Container

--- Meu Banquinho/MasterViewController.m
+++ Meu Banquinho/MasterViewController.m
@@ -47,14 +47,12 @@
     NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
     NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
     NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-
     [newManagedObject setValue:[self formataData] forKey:@"timeStamp"];
     NSLog(@"%@",[NSDate date]);
     
     NSError *error = nil;
     if (![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        // trata erro.
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
